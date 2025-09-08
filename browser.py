fd8810c980e2380af8947d1595f2d60b84536801
A.Ivanov <sasha1_00@mail.ru>
2025-09-08 22:33:29 +0300

    Add test_search

--- browser.py
+++ browser.py
@@ -1,8 +1,14 @@
 from selene import browser, be, have
 
+def test_search():
+    browser.open('/')
+    browser.element('#text').should(be.blank).type('qa.guru').press_enter()
+    browser.element('#search-result').should(have.text('QA.GURU'))
+
+
 def test_search_no_results():
 
-    browser.open('/')
+    browser.open('https://google.com/')
     search_box = browser.element('[name="q"]')
     search_box.should(be.visible).should(be.blank)
     search_box.type('qlkjdlk')
--- conftest.py
+++ conftest.py
@@ -1,20 +1,19 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selene import browser
+from selene import browser, be, have
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_settings():
-    # Настраиваем Firefox
     firefox_options = Options()
     driver = webdriver.Firefox(options=firefox_options)
 
-    driver.set_window_size(1200, 800)
     browser.config.driver = driver
-    browser.config.base_url = 'https://google.com'
-    browser.config.timeout = 10
+    browser.config.base_url = 'https://ya.ru'
+    browser.config.timeout = 15
+    browser.config.window_width = 1200
+    browser.config.window_height = 800
 
     yield
-
     browser.quit()
